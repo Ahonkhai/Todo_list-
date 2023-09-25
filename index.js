228608bce2603dbaaf1ae3a9e9baca5cc4f6e8ca
Ahonkhai <kakashiblack8@gmail.com>
2023-09-25 21:07:25 +0100

    added more stuff

--- index.js
+++ index.js
@@ -12,6 +12,46 @@ document.body.addEventListener("keydown", () => {
     console.log("enter")
 }})
 
+function get_time() {
+  var Time = new Date();
+  var hours = Time.getHours();
+  var minutes = Time.getMinutes();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes
+  }
+
+  if (hours > 12) {
+    hours = hours - 12;
+  }
+
+  var time_text = hours + ":" + minutes;
+
+  var time = time_text 
+
+  return time
+}
+
+var time = get_time();
+console.log(time)
+
+function get_date() {
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth();
+  var year = date.getFullYear();
+
+  var date_text = day + "/" + month + "/" + year;
+
+  return date_text
+}
+
+var date = get_date();
+console.log(date)
+
 
 function add() {
     // Create a container
@@ -22,12 +62,18 @@ function add() {
     
         // Created a paragraph that contains the item from the list 
         var paragraph = document.createElement("input")
-        paragraph.value = inputfield.value
+        paragraph.value = inputfield.value 
         paragraph.type = "text"
         paragraph.readOnly = true
 
         paragraph.id = "Para"
     
+        // Created a span that contains the time
+        var span = document.createElement("span")
+        span.innerText = get_time() + " " + get_date()
+
+        span.id = "time"
+
         // Created a remove button 🗑️
         var button = document.createElement("button")
         button.innerText = "Remove"
@@ -49,6 +95,7 @@ function add() {
         container.appendChild(paragraph)
         container.appendChild(edit_button)    
         container.appendChild(button)
+        container.appendChild(span)
         // document.body.appendChild(paragraph)
         // document.body.appendChild(button)
         document.body.appendChild(container)
@@ -58,6 +105,7 @@ function add() {
     
         button.addEventListener("click", function(){
             container.removeChild(paragraph)
+            container.removeChild(span)
             container.removeChild(button)
             container.removeChild(edit_button)
             container.style.margin = "0px"
@@ -87,12 +135,16 @@ function getCSSVariableValue(root) {
   // Usage
   const primaryColor = getCSSVariableValue('--primary-color');
   const secondaryColor = getCSSVariableValue('--secondary-color');
-  const extra = getCSSVariableValue('--extra-color')
-  
+  const extra = getCSSVariableValue('--extra-color');
+  const background = getCSSVariableValue('--background-btn');
+  const background_hover = getCSSVariableValue('--background-btn-hover');
+
   console.log('Primary Color:', primaryColor); 
   console.log('Secondary Color:', secondaryColor); 
   console.log('extra:', extra);
-  
+  console.log('background:', background);
+  console.log('background_hover:', background_hover);
+
 var dark = false;
 var rotated = false
 
